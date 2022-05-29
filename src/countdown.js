eecce5506e1c7eb1c07f76be43938f150d4bc895
Christopher Richardson <cwr@cwrichardson.com>
2022-05-29 18:46:15 +0200

    Remove check for DOM ready, as we load this defer in the theme.

--- src/countdown.js
+++ src/countdown.js
@@ -1,16 +1,14 @@
 import { render } from '@wordpress/element';
 import Countdown from 'react-countdown';
 
-window.addEventListener('DOMContentLoaded', () => {
-	const placeholders = document.getElementsByClassName(
-		'cwr-countdown-block-placeholder'
-	);
+const placeholders = document.getElementsByClassName(
+	'cwr-countdown-block-placeholder'
+);
 
-	for (const placeholder of placeholders) {
-		const data = JSON.parse(placeholder.querySelector('pre').innerText);
-		render(<CountDown {...data} />, placeholder);
-	}
-});
+for (const placeholder of placeholders) {
+	const data = JSON.parse(placeholder.querySelector('pre').innerText);
+	render(<CountDown {...data} />, placeholder);
+}
 
 export function CountDown(data) {
 	return (
